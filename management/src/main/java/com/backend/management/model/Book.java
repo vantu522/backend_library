3f690f79851cfa05ca0f52340cfe124b633cf3d5
tdv <tuduongvan2305@gmail.com>
2024-10-23 12:43:16 +0700

    first commit

--- management/src/main/java/com/backend/management/model/Book.java
+++ management/src/main/java/com/backend/management/model/Book.java
@@ -1,15 +1,16 @@
 package com.backend.management.model;
 
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document( collection = "books")
 public class Book {
-
+    @Id
     private String id;
     private String name;
     private String author;
-    private String category;
+    private String bigCategory;
     private int quanlity;
     private boolean availability;
 
