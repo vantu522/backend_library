d452e43fbf4f70442862d1ff5d46d006e223c97c
tdv <tuduongvan2305@gmail.com>
2024-10-24 14:48:33 +0700

    up

--- management/src/main/java/com/backend/management/model/Book.java
+++ management/src/main/java/com/backend/management/model/Book.java
@@ -1,17 +1,22 @@
 package com.backend.management.model;
 
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
 
-@Document( collection = "books")
+import java.util.List;
+
+@Data
+@Document(collection = "boooks")
 public class Book {
     @Id
-    private String id;
-    private String name;
-    private String author;
-    private String bigCategory;
-    private int quanlity;
-    private boolean availability;
+    private String idBook;
 
+    private String name;
+    private String description;
+    private List<String> bigCategory;
+    private List<String> smallCategory;
+    private List<String> author;
+    private String NXB;
+    private String img;
 }
