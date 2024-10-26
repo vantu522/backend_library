016e8b4dae8c22913c6270b97cbd678b097d064f
MinhQuang0611 <155757245+MinhQuang0611@users.noreply.github.com>
2024-10-26 22:58:56 +0700

    update2

--- management/src/main/java/com/backend/management/model/Category.java
+++ management/src/main/java/com/backend/management/model/Category.java
@@ -1,9 +1,39 @@
 package com.backend.management.model;
 
-public enum Category {
-    FICTION,
-    NON_FICTION,
-    SCIENCE,
-    HISTORY,
-    TECHNOLOGY
+import java.util.List;
+
+public class Category {
+    private String id;
+    private String name;
+    private List<Category> subCategories;
+
+    public Category(String id, String name, List<Category> subCategories) {
+        this.id = id;
+        this.name = name;
+        this.subCategories = subCategories;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Category> getSubCategories() {
+        return subCategories;
+    }
+
+    public void setSubCategories(List<Category> subCategories) {
+        this.subCategories = subCategories;
+    }
 }
--- management/src/main/java/com/backend/management/repository/BookRepo.java
+++ management/src/main/java/com/backend/management/repository/BookRepo.java
@@ -2,6 +2,18 @@ package com.backend.management.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.backend.management.model.Book;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface BookRepo extends MongoRepository<Book, String> {
+    // Tim sach theo ten
+    List<Book> findByNameOfBook( String name);
+    //Tim sach theo ten tac gia
+    List<Book> findByNameOfAuthor (String author);
+    //Tim sach theo nam xuat ban
+    List<Book> findByPublicationYear (Integer year);
+    // Tim sach theo the loai
+    List<Book> findBySubCategory(String subCategoryNames);
+
 }
