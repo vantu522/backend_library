e2ae0bf2465a9caf44a3b676c4933590a149293a
tdv <tuduongvan2305@gmail.com>
2024-10-27 22:11:06 +0700

    P

--- management/src/main/java/com/backend/management/controller/BookController.java
+++ management/src/main/java/com/backend/management/controller/BookController.java
@@ -3,30 +3,39 @@ package com.backend.management.controller;
 import com.backend.management.model.Book;
 import com.backend.management.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")  // Thêm version cho API
-@CrossOrigin(origins = "*") // Cho phép CORS
+@RequestMapping("/books")
 public class BookController {
-
     @Autowired
     private BookService bookService;
 
     @GetMapping
-    public List<Book> getAllBooks(){
+    public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
-    @GetMapping("/{idBook}")
-    public ResponseEntity<Book> getBookId(@PathVariable String idBook){
-        return bookService.getBookId(idBook)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping("/{id}")
+    public Optional<Book> getBookById(@PathVariable String id) {
+        return bookService.getBookById(id);
     }
 
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
 
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable String id, @RequestBody Book updatedBook) {
+        return bookService.updateBook(id, updatedBook);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable String id) {
+        bookService.deleteBook(id);
+    }
 }
